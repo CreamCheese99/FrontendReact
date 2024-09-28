ab9c08b8ac91acb5c4ecb6c75102fd5aa786ca3a
CreamCheese099 <thanchira50045@gmal.com>
2024-09-29 01:32:54 +0700

    login/insert

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter,RouterProvider,Route,Link,} from "react-router-dom"
+
+import Login from './pages/Login.jsx'
+import Insert from './pages/Insert.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "Insert",
+    element: <Insert />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
